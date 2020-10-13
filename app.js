d54ce6f8d376f42c7c9e760cbb38292031a2fd32
Karan Singh <ksingh1@cs.iitr.ac.in>
2020-10-13 14:06:29 +0530

    login completed

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const expresslayouts=require('express-ejs-layouts');
 const mongoose=require('mongoose');
 const flash = require('connect-flash');
 const session=require('express-session');
+const passport=require('passport'); 
 
 const app=express();
+
+//passport config
+require('./config/passport')(passport);
 
 //db config
 const db=require('./config/keys').MongoURI;
@@ -36,6 +40,10 @@ app.use(session({
     
   }));
 
+  //Passport middleware
+  app.use(passport.initialize());
+  app.use(passport.session());
+
   //connect flash
   app.use(flash());
   
@@ -43,6 +51,7 @@ app.use(session({
   app.use((req,res,next)=>{
       res.locals.success_msg=req.flash('success_msg');
       res.locals.error_msg=req.flash('error_msg');
+      res.locals.error=req.flash('error');
       next();
   });
 
